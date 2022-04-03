ec9d218b911211ff9d4b09c6c157288c4b59c551
Domn Werner <domn.werner@gmail.com>
2022-04-03 12:16:56 -0700

    Remove unused extension method.

--- PokeStats.Lib/AsyncExtensions.cs
+++ PokeStats.Lib/AsyncExtensions.cs
@@ -2,8 +2,7 @@
 
 public static class AsyncExtensions
 {
-    public static async Task<IEnumerable<T>> WhenAll<T>(this IEnumerable<Task<T>> tasks, CancellationToken cancellationToken = default) =>
-        await Task.WhenAll(tasks);
-
-    public static async Task<Option<U>> Bind<T, U>(this Task<Option<T>> task, Func<T, Option<U>> f) => (await task).Bind(f);
+    public static async Task<IEnumerable<T>> WhenAll<T>(
+        this IEnumerable<Task<T>> tasks, CancellationToken cancellationToken = default) =>
+            await Task.WhenAll(tasks);
 }
